7d0218a9a78e116c4fbc41c5dabf68a98ff25b64
sj2903 <sj2903@mail.ru>
2013-03-25 18:50:35 +0300

    Added mygets function

--- 2.c
+++ 2.c
@@ -1,9 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#define Max_num_of_stud 20
 
 typedef struct {
-    typedef struct{
+	typedef struct{
 		char name[10];
 		char surname[10];
 		char patronymic[10];
@@ -14,8 +15,24 @@ typedef struct {
 	} results_t;
 }students_t; 
 
+char* myfgets(char *str, int num)
+{
+	if( !(fgets(str, num, stdin)) ){
+		return 0;
+	}
+	else {
+	str[strlen(str)-1]='\0';
+	}
+	return str;
+}
+
 int main()
 {
+	int i;
+	for(i=0;i<Max_num_of_stud;i++){
+		printf("Enter the name of the %d student", i+1);
+	}
+	
     
     return 0;
 }
