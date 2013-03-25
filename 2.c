1c62f7b43f20c955ccc8ba60403adf08fd6f9599
sj2903 <sj2903@mail.ru>
2013-03-25 18:43:31 +0300

    Added structure init

--- 2.c
+++ 2.c
@@ -2,6 +2,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+typedef struct {
+    typedef struct{
+		char name[10];
+		char surname[10];
+		char patronymic[10];
+	} Full_name_t;
+	typedef union {
+		int exm_num;
+		int result;
+	} results_t;
+}students_t; 
+
 int main()
 {
     
