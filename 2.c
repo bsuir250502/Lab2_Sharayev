03670bfef53f9bb45a2cd25483227c9ccc8f40ce
sj2903 <sj2903@mail.ru>
2013-03-25 19:00:34 +0300

    Added read name function

--- 2.c
+++ 2.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <malloc.h>
 #define Max_num_of_stud 20
 
 typedef struct {
-	typedef struct{
+	 struct full_name_t{
 		char name[10];
 		char surname[10];
 		char patronymic[10];
-	} Full_name_t;
-	typedef union {
+	} full_name;
+	union results_t{
 		int exm_num;
 		int result;
-	} results_t;
+	} results;
 }students_t; 
 
 char* myfgets(char *str, int num)
@@ -26,12 +27,33 @@ char* myfgets(char *str, int num)
 	return str;
 }
 
+students_t* read_names(int *n)
+{
+    int i, j;
+    students_t *stud;
+    char arr[Max_num_of_stud][30];
+    for (i = 0; i < Max_num_of_stud; i++) {
+		printf("Enter the name of the %d student", i+1);
+        myfgets(arr[i], 30);
+        if (strcmp(arr[i],"end")) 
+        {
+            break;
+        }
+    }
+    *n = i;
+    stud = (students_t *) malloc(*n * sizeof(students_t));
+    for (i = 0; i < *n; i++) 
+    {
+        strncpy(stud[i].full_name.name, arr[i],30);
+    }
+    return stud;
+}
+
+
 int main()
 {
-	int i;
-	for(i=0;i<Max_num_of_stud;i++){
-		printf("Enter the name of the %d student", i+1);
-	}
+	int i,n=Max_num_of_stud;
+	read_names(&n);
 	
     
     return 0;
