65fb38b876512f34894ca2db099ee77250fd49ec
sj2903 <sj2903@mail.ru>
2013-04-03 14:56:40 +0400

    Added input number in range function

--- mylib.c
+++ mylib.c
@@ -27,3 +27,20 @@ int print_manual(int argc,char **argv){
 
     return 0;
 }
+
+int input_number_in_range(int from, int to)
+{
+    int n;
+    char *endptr;
+    char input_buffer[128];
+    int first = 1;
+    do {
+        if(!first){
+            printf("Please enter number between %d and %d", from, to);
+        }
+        myfgets(input_buffer, 128);
+        n = strtol(input_buffer, &endptr, 10);
+        first = 0;
+    } while( *endptr || n < from || n > to);
+    return n;
+}
