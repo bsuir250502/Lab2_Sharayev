a7731e9c5ef6dc8d6a3f8149483e32f7fd5703fc
sj2903 <sj2903@mail.ru>
2013-04-03 16:01:01 +0400

    Added tabulation

--- mylib.c
+++ mylib.c
@@ -13,16 +13,17 @@ char* myfgets(char *str, int num)
 
 int print_manual(int argc,char **argv){
     int i;
+    
     char input_buffer[128];
 	if(argc > 1 && !(strcmp(argv[1] ,"-h")) ) {
         FILE *fp=fopen("D:\Manual.txt","r");
-		while(fgets(input_buffer,strlen(input_buffer),fp))
-		{
-			printf("%s",input_buffer);
-		}
-		printf("\n");
-		fclose(fp);
-		return 1;
+        while(fgets(input_buffer,strlen(input_buffer),fp))
+        {
+            printf("%s",input_buffer);
+        }
+        printf("\n");
+        fclose(fp);
+        return 1;
     }
 
     return 0;
