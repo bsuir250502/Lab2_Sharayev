8e41ccbf617fe192aab992829a61480f7445f756
sj2903 <sj2903@mail.ru>
2013-04-03 14:59:25 +0400

    Fixed Bug

--- 2.c
+++ 2.c
@@ -97,7 +97,7 @@ int read_results(students_t * stud, int numb_of_stud, int sem_numb) {
 	for(i=0; i < numb_of_stud; i++) {
 		printf("%d) %s %s\n",i+1,stud[i].full_name.name, stud[i].full_name.surname);
 		for(j=0; j < amount_of_exams; j++) {
-			printf("  %s", catalog_of_exams(exam_numb_in_sem[sem_numb][j]));
+			printf("  %s", catalog_of_exams(exam_numb_in_sem[sem_numb-1][j]));
 			if(sem_numb == 1) {
 				stud[i].results.sem1_result[j]=input_number_in_range(1,10);
 			}
