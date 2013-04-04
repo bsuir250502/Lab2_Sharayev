5be0d8bd34646728604cf2cecefd0b29e4b4e4b1
sj2903 <sj2903@mail.ru>
2013-04-04 16:50:33 +0300

    Fixed mistake

--- mylib.c
+++ mylib.c
@@ -12,11 +12,9 @@ char* myfgets(char *str, int num)
 }
 
 int print_manual(int argc,char **argv){
-    int i;
-    
     char input_buffer[128];
 	if(argc > 1 && !(strcmp(argv[1] ,"-h")) ) {
-        FILE *fp=fopen("D:\Manual.txt","r");
+        FILE *fp=fopen("Manual.txt","r");
         while(fgets(input_buffer,strlen(input_buffer),fp))
         {
             printf("%s",input_buffer);
