708f6c3c4b0bcb42f0a8970542df33841c40c198
sj2903 <sj2903@mail.ru>
2013-04-08 14:53:04 +0300

    Amount of exams is array now

--- 2.c
+++ 2.c
@@ -93,13 +93,13 @@ students_t *read_full_names(int *num_of_stud)
 }
 
 int read_results(students_t * stud, int numb_of_stud, int sem_numb) {
-	int i, j, amount_of_exams = sem_numb+2, exam_numb_in_sem[][5]={{1, 2, 3}, {1, 2, 4, 5},{1, 2, 3, 4, 5}};
+	int i, j, amount_of_exams[3]={3, 4, 5}, exam_numb_in_sem[][5]={{1, 2, 3}, {1, 2, 4, 5},{1, 2, 3, 4, 5}};
 	printf("Specify the results of students:\n");
 	for(i=0; i < numb_of_stud; i++) {
 		printf("%d) %s %s\n",i+1,stud[i].full_name.name, stud[i].full_name.surname);
-		printf("Specify sem:\n");
+		printf("  Specify sem:  \n");
 		stud[i].number_of_sem=input_number_in_range(1,3);
-		for(j=0; j < amount_of_exams; j++) {
+		for(j=0; j < amount_of_exams[stud[i].number_of_sem - 1]; j++) {
 			printf("  %s  ", catalog_of_exams(exam_numb_in_sem[sem_numb-1][j]));
 			switch (stud[i].number_of_sem) {
 				case 1: 
@@ -120,11 +120,11 @@ int read_results(students_t * stud, int numb_of_stud, int sem_numb) {
 }
 
 int print_information(students_t * stud, int numb_of_stud, int sem_numb) {
-	int i, j, amount_of_exams = sem_numb+2, exam_numb_in_sem[][4]={{1, 2, 3}, {1, 2, 4, 5}};
+	int i, j, amount_of_exams[3]={3, 4, 5}, exam_numb_in_sem[][4]={{1, 2, 3}, {1, 2, 4, 5}};
 	printf("Results of students:\n");
 	for(i=0; i < numb_of_stud; i++) {
 		printf("%d) %s %s\n",i+1,stud[i].full_name.name, stud[i].full_name.surname);
-		for(j=0; j < amount_of_exams; j++) {
+		for(j=0; j < amount_of_exams[stud[i].number_of_sem - 1]; j++) {
 			printf("  %s  ", catalog_of_exams(exam_numb_in_sem[sem_numb-1][j]));
 			if(sem_numb == stud[i].number_of_sem) {
 				switch (stud[i].number_of_sem) {
@@ -146,7 +146,6 @@ int print_information(students_t * stud, int numb_of_stud, int sem_numb) {
 
 	return 0;
 }
-
 
 char *catalog_of_exams(int exam_number) {
     static char exam[][12]={"Math", "Programming", "AiLOVT", "Physics", "English"};
